db9aa37e5a7034b12bbee33f280567ffe83f9522
Jashwanth <122328653+WeCanCodeForLife@users.noreply.github.com>
2023-05-01 12:19:46 -0700

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -22,7 +22,7 @@ const Categories = ({ selectedCategory, setSelectedCategory }) => (
         }}
         key={category.name}
       >
-        <span style={{ color: category.name === selectedCategory ? "white" : "#97BC62FF", marginRight: "15px" }}>
+        <span style={{ color: category.name === selectedCategory ? "white" : "#041014", marginRight: "15px" }}>
           {category.icon}
         </span>
         <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
